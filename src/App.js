55b37f1520856f9cab95039e9c925d5cf2496e42
Siva <sivansbhfe@gmail.com>
2019-10-06 15:11:09 +0530

    Added inline styling for header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from './Header'
+import Header from './Header';
 
 function App() {
 
   return (
    <div>
         <Header />
+        
      <button>Add</button>
      <div>
        <span>Name:</span><br></br>
@@ -15,4 +16,14 @@ function App() {
   );
 }
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <p>UpGrad - Building Careers of Tomorrow!</p>
+//         <App/>
+//       </div>
+//     );
+//   }
+// }
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,9 @@
 import React from 'react';
 const Header = function(){
-    return (<div className="header">
+    const headerStyle={textAlign: 'center',padding: 20,background: '#000',color: '#fff',textTransform: 'uppercase'};
+    return (<div style={headerStyle}>
     Phone Directory
 </div>);
 }
-// class Header extends Component{
-//     render(){
-//         return (<div className="header">
-//             Phone Directory
-//          </div>);
-//         }
-// }
+
 export default Header;
