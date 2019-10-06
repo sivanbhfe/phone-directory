9240e5430e0415fc7e6ff0056517116008c158b0
Siva <sivansbhfe@gmail.com>
2019-10-06 15:18:47 +0530

    Transferred header styling from inline to external

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Header.css';
 const Header = function(){
-    const headerStyle={textAlign: 'center',padding: 20,background: '#000',color: '#fff',textTransform: 'uppercase'};
-    return (<div style={headerStyle}>
+   // const headerStyle={textAlign: 'center',padding: 20,background: '#000',color: '#fff',textTransform: 'uppercase'};
+    return (<div className='header'>
     Phone Directory
 </div>);
 }
